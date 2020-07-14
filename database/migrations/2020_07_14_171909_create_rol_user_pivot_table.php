c02310b73bcdcd648c419867713864470523dc8f
Nacxit <nacxitArmando@hotmail.com>
2020-07-14 16:15:32 -0600

    create buddings and comments migrations

---
+++ database/migrations/2020_07_14_171909_create_rol_user_pivot_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateRolUserPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rol_user', function (Blueprint $table) {
+            $table->foreignId('rol_id')->references('id')->on('rols')->onDelete('cascade');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->primary(['rol_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rol_user');
+    }
+}
---
+++ database/migrations/2020_07_14_173137_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->boolean('active')->default(1);
+            $table->foreignId('auctions_id')->references('id')->on('auctions')->onDelete('cascade')->nullable();
+            $table->foreignId('sells_id')->references('id')->on('sells')->onDelete('cascade')->nullable();
+            $table->foreignId('users_id')->references('id')->on('users')->onDelete('cascade');
+            $table->id();
+            $table->text('comments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
