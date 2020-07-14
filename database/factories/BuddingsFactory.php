4db72930c5d2ed0ceadd1035920274ceb44ccd60
Nacxit <nacxitArmando@hotmail.com>
2020-07-14 16:08:44 -0600

    create buddings and comments migrations

---
+++ database/factories/BuddingsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\buddings;
+use Faker\Generator as Faker;
+
+$factory->define(buddings::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
