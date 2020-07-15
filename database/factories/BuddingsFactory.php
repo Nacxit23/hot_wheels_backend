08519e0232a634e118fd0c3137e85d9404fccade
Nacxit <nacxitArmando@hotmail.com>
2020-07-15 16:26:07 -0600

    create budding factory

--- database/factories/BuddingsFactory.php
+++ database/factories/BuddingsFactory.php
@@ -6,7 +6,15 @@ use App\Models\buddings;
 use Faker\Generator as Faker;
 
 $factory->define(buddings::class, function (Faker $faker) {
+
+    $auctions = \App\Models\auction::find(1);
+    $users = $auctions->users;
+
     return [
-        //
+        'date_budding' => $faker->dateTime,
+        'price' => $faker->randomNumber(2),
+        'users_id' => $faker->randomElement($users),
+        'auction_id' => 1,
+        'comments' => $faker->text
     ];
 });
