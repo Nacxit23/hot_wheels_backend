56478bcec8254f92cced2668e3fa92aa482cdebe
Nacxit <nacxitArmando@hotmail.com>
2020-07-24 18:08:37 -0600

    update rol and users

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'nameUser', 'email', 'password', 'active', 'genre', 'date_birth', 'city', 'first_name',
-        'identification', 'last_name', 'name', "address", "phone_number"
+        'identification', 'last_name', 'name', "address", "phone_number",'token'
     ];
 
     /**
