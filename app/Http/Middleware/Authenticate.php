9aa3f440093eec5f7b51229c4540467d17b6fb77
Nacxit <nacxitArmando@hotmail.com>
2020-08-03 19:14:51 -0600

    Create authentication

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,19 +3,21 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Auth;
+use Nuwave\Lighthouse\Exceptions\AuthenticationException;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return string|null
      */
-    protected function redirectTo($request)
+    protected function authenticate($request, array $guards)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (!$this->auth->guard()->check()) {
+            throw new AuthenticationException('Unauthenticated.', $guards);
         }
     }
 }
